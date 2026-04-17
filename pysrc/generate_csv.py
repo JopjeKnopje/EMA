bbdb13174feef0f01657c689d1a81d1ac45ab4c9
Joppe Boeve <jboeve@f1r3s7.codam.nl>
2026-04-17 15:36:30 +0200

    Add generator script

---
+++ pysrc/generate_csv.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from random import randrange
+
+from cyclopts import App
+
+app = App()
+
+
+
+def write_to_file(file: Path, data: list[int]) -> None:
+    str_data = [str(x) for x in data]
+    print(str_data)
+    with file.open("w") as f:
+        for sample in str_data:
+            f.writelines(f"{sample}\n")
+
+
+
+@app.command
+@app.default
+def random(n: int = 32, min: int = 0, max: int = 256, file: Path | None = None) -> list[int] | None:
+    """
+    Generate `n` amount of random integers 
+    """
+    samples: list[int] = []
+    for i in range(n):
+        samples.append(randrange(min, max))
+
+    if file is not None:
+        write_to_file(file, samples)
+    else:
+        return samples
+
+app()
